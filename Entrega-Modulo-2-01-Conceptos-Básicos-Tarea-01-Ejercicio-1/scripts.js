8948cffb6499bbe24fd86b5ea0e2dc4abbf10bd2
Ohtico <viticogelvis5@gmail.com>
2021-07-26 09:26:32 -0500

    modulo 2_01

---
+++ Entrega-Modulo-2-01-Conceptos-Básicos-Tarea-01-Ejercicio-1/scripts.js
@@ -0,0 +1,14 @@
+
+//declaramos las variables
+let numero1;
+let numero2;
+let Suma;
+
+// le asignaremos el valor de cada variable
+
+numero1 = 5;
+numero2 = 7;
+
+Suma = numero2 + numero1;
+
+alert('La suma de ' + numero1 + ' + ' + numero2 + ' es: ' + Suma)
